25b8498583aea42270b3a0320642abd54cfb2c02
Mark Chouinard <mark@chouinard.me>
2019-07-30 15:02:07 -0700

    Add table view with static cell data

--- Notes/ViewController.swift
+++ Notes/ViewController.swift
@@ -8,11 +8,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    @IBOutlet weak var table: UITableView!
+    
+    var data:[String] = ["Item 1", "Item 2", "Item 3"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        table.dataSource = self
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell:UITableViewCell = UITableViewCell()
+        cell.textLabel?.text = data[indexPath.row]
+        return cell
     }
 
 
